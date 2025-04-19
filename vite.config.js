b47ba14751fa8d61638d25505c8c46f64227857c
Vladi-Maxx <storemaxmark@gmail.com>
2025-04-19 17:23:35 +0300

    chore: fix dev server port to 5173

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true
+    open: true,
+    port: 5173
   }
 });
