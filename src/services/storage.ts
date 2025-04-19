64bfe0b8ea636931343a30a70f431c01826b46fc
Vladi-Maxx <storemaxmark@gmail.com>
2025-04-19 16:26:35 +0300

    chore: scaffold storage service (step 3.1)

---
+++ src/services/storage.ts
@@ -0,0 +1,14 @@
+// TODO: implement storage service
+
+export function loadFacts(): any[] {
+  // TODO: load facts from localStorage
+  return [];
+}
+
+export function saveFacts(facts: any[]): void {
+  // TODO: save facts to localStorage
+}
+
+export function logSession(session: any): void {
+  // TODO: log session to localStorage
+}
